fedaba27c2cecef7ab3ddc9b317f6c9778eb775a
dxia2 <90279328+dxia2@users.noreply.github.com>
2021-09-12 14:44:31 -0600

    Add files via upload

---
+++ kf-panda-character-search-master/kf-panda-character-search-master/script.js
@@ -0,0 +1,16 @@
+//kung fu panda character search
+
+//get input
+let name = prompt("Enter a name:");
+if(name === "Po"){
+    alert("Yes, Po is a kf panda character");
+}else if(name === "Viper"){
+    alert("Yes, Viper is a kf panda character");
+}else if(name === "Mantis"){
+    alert("Yes, Mantis is a kf panda character");
+}else if(name === "Crane"){
+    alert("Yes, Crane is a kf panda character");
+}else{
+    alert("That is not a kungfu panda character");
+}
+
